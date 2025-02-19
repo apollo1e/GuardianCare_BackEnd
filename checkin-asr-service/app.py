a764b359fd5b031d21f7b4bbf63b73213db7103c
Vignesh <bala12rupesh@gmail.com>
2025-02-19 19:01:04 +0800

    Can receive and transcribe audio

---
+++ checkin-asr-service/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, Response
+from flask_cors import CORS
+import whisper
+import os
+from utils import *
+
+app = Flask(__name__)
+CORS(app)
+
+global asr_model
+
+@app.route('/stream', methods=['POST'])
+def stream():
+    data = request.get_data()
+    print(f"Got {len(data)} I2S bytes")
+    
+    with open('./data/i2s.raw', 'ab') as f:
+        f.write(data)
+    
+    return 'OK'
+
+@app.route('/end_stream', methods=['POST'])
+def end_stream():
+    print("End of transmission")
+    
+    try:
+        with open('./data/i2s.raw', 'rb') as f:
+            raw_data = f.read()
+        convert_raw_to_wav(raw_data)
+        os.remove('./data/i2s.raw')
+            
+        print("Transcribing audio...")
+        large_result = asr_model.transcribe("./data/audio.wav", language="en", fp16=False, verbose=True)
+        print("Transcription:")
+        print(large_result["text"])
+            
+        return 'OK'
+        
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return Response(f'Error: {str(e)}', status=500)
+
+if __name__ == '__main__':
+    print("Loading model...")
+    asr_model = whisper.load_model("large")
+    print("Model loaded!")
+    app.run(host=FLASK_HOST, port=FLASK_PORT,
+            threaded=FLASK_THREADED, debug=FLASK_DEBUG)
---
+++ checkin-asr-service/config.py
@@ -0,0 +1,7 @@
+# Configurations
+FLASK_HOST = "0.0.0.0"                                              # Flask host
+FLASK_PORT = "3000"                                                 # Flask port
+FLASK_THREADED = True                                               # Flask threaded option
+FLASK_DEBUG = True                                                  # Flask debug option
+M5STICK_MIC_GAIN = 10
+M5STICK_MIC_SAMPLE_RATE = 4000
