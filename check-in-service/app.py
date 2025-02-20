4c727a98988fad20e6c91323b08a4008e3eef537
Vignesh <bala12rupesh@gmail.com>
2025-02-20 14:47:32 +0800

    Changed routes

--- check-in-service/app.py
+++ check-in-service/app.py
@@ -1,59 +1,19 @@
 # TODO: add a timeout on transcriptions to avoid blocking the server
 # TODO: use whisper.cpp to transcribe the audio, find out how to serve multiple clients at once
 
-from flask import Flask, request, Response
+from flask import Flask
 from flask_cors import CORS
-import whisper
-import os
-from utils import *
+from blueprints.m5stick import m5stick_bp
+from blueprints.mobile import mobile_bp
+from config import *
 
 app = Flask(__name__)
 CORS(app)
 
-global asr_model
-
-@app.route('/stream', methods=['POST'])
-def stream():
-    data = request.get_data()
-    print(f"Got {len(data)} I2S bytes")
-    
-    raw_file_path = './data/i2s.raw'
-    with open(raw_file_path, 'ab') as f:
-        f.write(data)
-    
-    return 'OK'
-
-@app.route('/end_stream', methods=['POST'])
-def end_stream():
-    print("End of transmission")
-    
-    try:
-        raw_file_path = './data/i2s.raw'
-        with open(raw_file_path, 'rb') as f:
-            raw_data = f.read()
-        wav_file_path = convert_raw_to_wav(raw_data)
-        os.remove(raw_file_path)
-            
-        print("Transcribing audio...")
-        result = asr_model.transcribe(wav_file_path, language="en", fp16=False)
-        transcription = result["text"]
-        print("Transcription:")
-        print(transcription)
-        
-        print("Processing transcription...")
-        json_response = process_transcription(transcription)
-        print("Response:")
-        print(json_response)
-            
-        return 'OK'
-        
-    except Exception as e:
-        print(f'Error: {str(e)}')
-        return Response(f'Error: {str(e)}', status=500)
+app.register_blueprint(m5stick_bp, url_prefix="/m5stick")
+app.register_blueprint(mobile_bp, url_prefix="/mobile")
 
 if __name__ == '__main__':
-    print("Loading model...")
-    asr_model = whisper.load_model("large")
-    print("Model loaded!")
+    print(app.url_map)
     app.run(host=FLASK_HOST, port=FLASK_PORT,
             threaded=FLASK_THREADED, debug=FLASK_DEBUG)
---
+++ check-in-service/blueprints/m5stick.py
@@ -0,0 +1,49 @@
+import os
+import whisper
+from flask import Blueprint, request, Response
+from utils import convert_raw_to_wav, process_transcription
+
+m5stick_bp = Blueprint('m5stick', __name__)
+
+print("Loading model...")
+asr_model = whisper.load_model("large")
+print("Model loaded!")
+
+@m5stick_bp.route('/audio/stream', methods=['POST'])
+def stream():
+    data = request.get_data()
+    print(f"Got {len(data)} I2S bytes")
+    
+    raw_file_path = './data/i2s.raw'
+    with open(raw_file_path, 'ab') as f:
+        f.write(data)
+    
+    return 'OK'
+
+@m5stick_bp.route('/audio/stop', methods=['POST'])
+def end_stream():
+    print("End of transmission")
+    
+    try:
+        raw_file_path = './data/i2s.raw'
+        with open(raw_file_path, 'rb') as f:
+            raw_data = f.read()
+        wav_file_path = convert_raw_to_wav(raw_data)
+        os.remove(raw_file_path)
+            
+        print("Transcribing audio...")
+        result = asr_model.transcribe(wav_file_path, language="en", fp16=False)
+        transcription = result["text"]
+        print("Transcription:")
+        print(transcription)
+        
+        print("Processing transcription...")
+        json_response = process_transcription(transcription)
+        print("Response:")
+        print(json_response)
+            
+        return 'OK'
+        
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return Response(f'Error: {str(e)}', status=500)
